07f154188df34a98c7389f12a80dbbcf2a9d86fd
Rodney Gauna <rodneygauna@gmail.com>
2023-06-12 19:00:26 +0000

    ability to view department details

--- src/settings/views.py
+++ src/settings/views.py
@@ -32,7 +32,7 @@ def settings_landing_page():
                            title="Settings")
 
 
-# Settings - Departments - View
+# Settings - Departments - View List of Departments
 @settings_bp.route("/settings/departments")
 @login_required
 @admin_required
@@ -51,6 +51,46 @@ def view_departments():
                            title="Departments",
                            departments=departments,
                            department_user_count=department_user_count)
+
+
+# Settings - Departments - View Department Details
+@settings_bp.route("/settings/departments/<int:department_id>")
+@login_required
+@admin_required
+def view_department_details(department_id):
+    """
+    View department details
+    """
+
+    # Get department
+    department = Departments.query.get_or_404(department_id)
+
+    # Get total count of users for each department
+    department_user_count = (
+        db.session.query(User.id)
+        .filter_by(department_id=department_id).count()
+    )
+
+    # Get user details for the department
+    department_users = (
+        db.session.query(
+            User.id,
+            User.firstname,
+            User.lastname,
+            User.email,
+            User.status,
+            User.department_id,
+        )
+        .filter_by(department_id=department_id)
+        .order_by(User.lastname)
+        .all()
+    )
+
+    return render_template("settings/view_department_details.html",
+                           title="Department Details",
+                           department=department,
+                           department_user_count=department_user_count,
+                           department_users=department_users)
 
 
 # Settings - Users
